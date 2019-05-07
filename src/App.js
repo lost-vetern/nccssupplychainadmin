22133cd94f574dbf0c38c8e46d0a805e78da798b
Amad Durrani <amadbarca@gmail.com>
2019-05-07 11:35:54 +0500

    Function to class

--- src/App.js
+++ src/App.js
@@ -2,24 +2,35 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <form>
-        <label>
-          Diamond Name:
-          <input type="text" name="name" />
-        </label>
-
-        <label>
-          Description:
-          <input type="text" name="name" />
-        </label>
-        
-        <input type="submit" value="Submit" />
-      </form>
-    </div>
-  );
-}
+class App extends React.Component {
+    constructor(props) {
+      super(props);
+      this.state = {value: ''};
+  
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+    }
+  
+    handleChange(event) {
+      this.setState({value: event.target.value});
+    }
+  
+    handleSubmit(event) {
+      alert('A name was submitted: ' + this.state.value);
+      event.preventDefault();
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input type="text" value={this.state.value} onChange={this.handleChange} />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      );
+    }
+  }
 
 export default App;
